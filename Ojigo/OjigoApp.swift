13f6d62b010890ae47609c0c38929bdba018fe3c
DJDrama <putzzang@naver.com>
2021-10-19 22:56:57 +0900

    Initial Commit

---
+++ Ojigo/OjigoApp.swift
@@ -0,0 +1,17 @@
+//
+//  OjigoApp.swift
+//  Ojigo
+//
+//  Created by dj on 2021/10/19.
+//
+
+import SwiftUI
+
+@main
+struct OjigoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
